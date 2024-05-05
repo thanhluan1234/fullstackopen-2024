ac2230d26ba8bfc482ea7ebce13ba4d2b34cb72a
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-05 21:37:04 +0300

    fix: unicafe step 5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,38 +6,70 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const handleAddGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleAddNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleAddBad = () => {
+    setBad(bad + 1);
+  };
+
   return (
     <>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={handleAddGood} text="good" />
+      <Button onClick={handleAddNeutral} text="neutral" />
+      <Button onClick={handleAddBad} text="bad" />
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
 
-const Statistics = (props) => {
-  if (props.good === 0 && props.neutral === 0 && props.bad === 0) {
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
     return <p>No feedback given</p>;
   }
 
   return (
     <>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>total {props.good + props.neutral + props.bad}</p>
-      <p>
-        avarage{" "}
-        {(props.good - props.bad) / (props.good + props.neutral + props.bad)}
-      </p>
-      <p>
-        positive {(props.good / (props.good + props.neutral + props.bad)) * 100}{" "}
-        %
-      </p>
+      <StatisticsLine text="good" value={good} />
+      <StatisticsLine text="neutral" value={neutral} />
+      <StatisticsLine text="bad" value={bad} />
+      <StatisticsLine text="total" value={good + neutral + bad} />
+      <StatisticsLine
+        text="avarage"
+        value={(good - bad) / (good + neutral + bad)}
+      />
+      <StatisticsLine
+        text="positive"
+        value={(good / (good + neutral + bad)) * 100}
+      />
     </>
+  );
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const StatisticsLine = ({ text, value }) => {
+  if (text === "positive") {
+    return (
+      <p>
+        {text} {value} %
+      </p>
+    );
+  }
+
+  return (
+    <p>
+      {text} {value}
+    </p>
   );
 };
 
