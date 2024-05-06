fb8ca56ea7bd9ee8b459b8b2b235b7f5b8bc3eda
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-06 23:41:49 +0300

    fix: restcountries step 3

--- part2/restcountries/src/App.jsx
+++ part2/restcountries/src/App.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 
 const ALL_COUNTRIES_URL =
   "https://studies.cs.helsinki.fi/restcountries/api/all";
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${WEATHER_API_KEY}&q=`;
+const WEATHER_ICON_URL = "https://openweathermap.org/img/wn";
 
 function App() {
   const [allCountries, setAllCountries] = useState([]);
@@ -58,31 +61,69 @@ const DisplayFound = ({ data, handleShowCountry }) => {
   }
 
   if (data.length === 1) {
-    return (
-      <div>
-        <h1>{data[0].name.common}</h1>
-        <p>Capital: {data[0].capital[0]}</p>
-        <p>Area: {data[0].area}</p>
-        <h2>Languages</h2>
-        <ul>
-          {Object.values(data[0].languages).map((language) => (
-            <li key={language}>{language}</li>
-          ))}
-        </ul>
-        <img src={data[0].flags.png} alt={`${data[0].name.common}-flag`} />
-      </div>
-    );
+    return <DisplayOneCountry data={data[0]} />;
   }
 
   return (
-    <div>
+    <>
       {data.map((country, idx) => (
         <div key={idx}>
           <p>{country.name.common}</p>
           <button onClick={() => handleShowCountry(idx)}>Show</button>
         </div>
       ))}
-    </div>
+    </>
+  );
+};
+
+const DisplayOneCountry = ({ data }) => {
+  return (
+    <>
+      <h1>{data.name.common}</h1>
+      <p>Capital: {data.capital[0]}</p>
+      <p>Area: {data.area}</p>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(data.languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={data.flags.png} alt={`${data.name.common}-flag`} />
+      <DisplayWeather name={data.capital[0]} />
+    </>
+  );
+};
+
+const DisplayWeather = ({ name }) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`${WEATHER_URL}${name}`)
+        .then((res) => {
+          setData(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [name]);
+
+  if (!data) {
+    return null;
+  }
+
+  return (
+    <>
+      <h2>Weather in {name}</h2>
+      <p>Temperature: {data.main.temp} °C</p>
+      <img
+        src={`${WEATHER_ICON_URL}/${data.weather[0].icon}@2x.png`}
+        alt="weather-icon"
+      />
+      <p>Wind: {data.wind.speed} m/s</p>
+    </>
   );
 };
 
