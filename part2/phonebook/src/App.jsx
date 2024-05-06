183fd6c75d9c2f67cedc401b385aff578c5d29ad
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-06 14:04:19 +0300

    fix: phonebook step 1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleAddNewPerson = (event) => {
+    event.preventDefault();
+    let newPersons = [...persons];
+    const newPerson = {
+      name: newName,
+    };
+    newPersons.push(newPerson);
+    setPersons(newPersons);
+    setNewName("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAddNewPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((i) => (
+        <p key={i.name}>{i.name} </p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
