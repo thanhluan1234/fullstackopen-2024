fb290a5bd3404a4cb6f84c6e42399c9e467e6a98
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-06 22:57:32 +0300

    fix: restcountries step 1

--- part2/restcountries/src/App.jsx
+++ part2/restcountries/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const ALL_COUNTRIES_URL =
+  "https://studies.cs.helsinki.fi/restcountries/api/all";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [allCountries, setAllCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [found, setFound] = useState([]);
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+
+    if (value !== "") {
+      const found = allCountries.filter((country) =>
+        country.name.common
+          .toLowerCase()
+          .includes(e.target.value.toLowerCase()),
+      );
+      setFound(found);
+      setSearchTerm(value);
+    } else {
+      setFound([]);
+      setSearchTerm("");
+    }
+  };
+
+  // Get countries at initial render
+  useEffect(() => {
+    axios
+      .get(ALL_COUNTRIES_URL)
+      .then((res) => {
+        setAllCountries(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Find country</p>
+      <input value={searchTerm} onChange={handleSearch} />
+      <DisplayFound data={found} />
     </>
-  )
+  );
 }
 
-export default App
+const DisplayFound = ({ data }) => {
+  if (data.length > 10) {
+    return <p>Too many matches, specify another filter</p>;
+  }
+
+  if (data.length === 1) {
+    return (
+      <div>
+        <h1>{data[0].name.common}</h1>
+        <p>Capital: {data[0].capital[0]}</p>
+        <p>Area: {data[0].area}</p>
+        <h2>Languages</h2>
+        <ul>
+          {Object.values(data[0].languages).map((language) => (
+            <li key={language}>{language}</li>
+          ))}
+        </ul>
+        <img src={data[0].flags.png} alt={`${data[0].name.common}-flag`} />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {data.map((country) => (
+        <p key={country.name.common}>{country.name.common}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
