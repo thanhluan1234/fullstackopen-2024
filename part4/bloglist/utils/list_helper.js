07c6f3e907a9c845bee2c0ca19468fad811a0fa2
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-08 21:34:28 +0300

    feat: helper function & unit tests step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0
+  return blogs.reduce((sum, i) => {
+    return sum + i.likes
+  }, sum)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
