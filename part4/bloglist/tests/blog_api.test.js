ac1aa602e0feed7dfd11e731041d0a1a61735683
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-09 15:01:55 +0300

    feat: bloglist tests step 1

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,63 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const app = require('../app')
+const mongoose = require('mongoose')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Title 1',
+    author: 'Author 1',
+    url: 'https://example.com/1',
+    likes: 8,
+  },
+  {
+    title: 'Title 2',
+    author: 'Author 2',
+    url: 'https://example.com/2',
+    likes: 2,
+  },
+  {
+    title: 'Title 3',
+    author: 'Author 2',
+    url: 'https://example.com/3',
+    likes: 7,
+  },
+  {
+    title: 'Title 4',
+    author: 'Author 2',
+    url: 'https://example.com/4',
+    likes: 6,
+  },
+  {
+    title: 'Title 5',
+    author: 'Author 1',
+    url: 'https://example.com/5',
+    likes: 10,
+  },
+  {
+    title: 'Title 6',
+    author: 'Author 1',
+    url: 'https://example.com/6',
+    likes: 7,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObjects = initialBlogs.map((blog) => new Blog(blog))
+  let promiseArray = blogObjects.map((blog) => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('count blogs in test env return as 6', async () => {
+  const res = await api.get('/api/blogs')
+  assert.strictEqual(res.body.length, 6)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,4 +1,9 @@
 const PORT = process.env.PORT || 3001
-const MONGODB_URI = process.env.MONGODB_URI || ''
+const MONGODB_URI =
+  process.env.NODE_ENV === 'prod'
+    ? process.env.PROD_MONGODB_URI
+    : process.env.NODE_ENV === 'dev'
+      ? process.env.DEV_MONGODB_URI
+      : process.env.TEST_MONGODB_URI
 
 module.exports = { PORT, MONGODB_URI }
