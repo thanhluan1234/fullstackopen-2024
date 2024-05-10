8692f79f47abc1a43872710e7ebedbee7ec2ee5b
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-10 15:56:16 +0300

    feat: bloglist frontend step 7

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: "solid",
+  borderWidth: 1,
+  marginBottom: 5,
+};
+
+const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title} {blog.author}
+      </p>
+      <button onClick={() => setIsVisible(!isVisible)}>
+        {isVisible ? "Hide" : "View"}
+      </button>
+      {isVisible ? (
+        <>
+          <p>{blog.url}</p>
+          <p>
+            {blog.likes} likes
+            <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+export default Blog;
