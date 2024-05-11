76e43c561190bca43113e6b43bf3b1ca4de5f4b6
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-11 23:09:21 +0300

    feat: blgolist frontend tests step 2

--- part5/bloglist/src/components/Blog.test.jsx
+++ part5/bloglist/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { test, expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
 import Blog from '../components/Blog';
 
 test('render blog', () => {
@@ -14,4 +15,26 @@ test('render blog', () => {
 
   const element = screen.getByText('Title 1 Author 1');
   expect(element).toBeDefined();
+});
+
+test("showing blog's url and likes when clicked on show button", async () => {
+  const blog = {
+    title: 'Title 1',
+    author: 'Author 1',
+    url: 'https://example.com/1',
+    likes: 0,
+    user: {
+      name: 'Tester',
+      username: 'tester',
+    },
+  };
+
+  render(<Blog blog={blog} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText('View');
+  await user.click(button);
+
+  expect(screen.getByText('https://example.com/1')).toBeDefined();
+  expect(screen.getByText('0 likes')).toBeDefined();
 });
