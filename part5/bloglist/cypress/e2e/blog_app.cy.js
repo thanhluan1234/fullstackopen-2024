e045a4ae23a61378aafdbcdfa052a72dea248929
Luan Nguyen <luan.nguyen@centria.fi>
2024-05-12 22:42:13 +0300

    feat: bloglist e2e testing step 3

--- part5/bloglist/cypress/e2e/blog_app.cy.js
+++ part5/bloglist/cypress/e2e/blog_app.cy.js
@@ -38,4 +38,28 @@ describe('blog app', () => {
       cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)');
     });
   });
+
+  describe("when logged in", () => {
+    beforeEach(() => {
+      cy.get('input[name="username"]').type('admin');
+      cy.get('input[name="password"]').type('adminpassword');
+      cy.get('button').contains('Login').click();
+      cy.get('p').contains('Admin logged in').should('be.visible');
+    });
+
+    it("a new blog can be created", () => {
+      cy.get('button').contains('New blog').click();
+      cy.get('input[name="title"]').type('Title 1');
+      cy.get('input[name="author"]').type('Author 1');
+      cy.get('input[name="url"]').type('https://example.com/1');
+      cy.get('button').contains('Create').click();
+
+      cy.get('p.success').contains('A new blog Title 1 by Author 1 added').should('be.visible');
+      cy.get('.success').should('have.css', 'color', 'rgb(0, 128, 0)');
+
+      cy.reload()
+
+      cy.get('p.title').contains('Title 1 Author 1').should('be.visible');
+    })
+  })
 });
